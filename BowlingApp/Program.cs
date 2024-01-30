ae15b7ef5b6eecbfa1a744dfa65d1bb5b323e7a9
Yousuf Hussain <yhussain8@gmail.com>
2024-01-30 00:59:52 -0500

    Update Game scoring mechanic for strikes

--- BowlingApp/Program.cs
+++ BowlingApp/Program.cs
@@ -11,19 +11,30 @@ namespace BowlingApp
         private int rollNumber = 0;
         private int frameScore = 0;
         private bool spare = false;
+        private int strike = 0;
         private bool bonus = false;
 
         public void Roll(int pins)
         {
             rollNumber++;
             score += pins;
+            frameScore += pins;
+            if (strike > 0)
+            {
+                score += pins;
+                strike--;
+            }
+            if (pins == 10)
+            {
+                strike = 2;
+                rollNumber++;
+            }
             if (rollNumber > 20)
             {
                 bonus = true;
             }
             if (rollNumber % 2 != 0)
             {
-                frameScore += pins;
                 if (spare && !bonus)
                 {
                     score += pins;
@@ -32,8 +43,7 @@ namespace BowlingApp
             }
             else
             {
-                frameScore += pins;
-                if (frameScore == 10)
+                if (frameScore == 10 && strike != 2)
                 {
                     spare = true;
                 }
